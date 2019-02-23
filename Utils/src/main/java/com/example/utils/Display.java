8991d1c6bae9d018cfab82513cb4c8bcec28561a
the gioi cua thanh <pham54833@gmail.com>
2019-02-23 10:29:54 +0700

    - start project : +add library display message +add library lib ,mylibrary

--- Utils/src/main/java/com/example/utils/Display.java
+++ Utils/src/main/java/com/example/utils/Display.java
@@ -1,7 +1,12 @@
 package com.example.utils;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Display {
@@ -17,4 +22,48 @@ public class Display {
         }
     }
 
+    /*public static void SnackBar(View view, String message,int colorBackground,int colorText,int sizeText){
+
+        if(message!=null&&view!=null){
+
+            Snackbar snackbar;
+            View viewSnackBar;
+            TextView textView;
+
+            snackbar=Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+            viewSnackBar=snackbar.getView();
+            viewSnackBar.setBackgroundColor(colorBackground);
+
+            textView=viewSnackBar.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(colorText);
+            textView.setTextSize(sizeText);
+
+            snackbar.show();
+        }
+
+
+    }*/
+
+   public class SnackBar{
+
+       private Snackbar snackbar;
+       private View viewSnackBar;
+       private TextView textView;
+       private View idView;
+
+
+       public SnackBar(View idView) {
+        this.idView=idView;
+       }
+
+       public void setMessage(String message){
+           if(message!=null){
+               Snackbar.make(idView, message, Snackbar.LENGTH_SHORT);
+           }
+       }
+
+
+
+
+   }
 }
--- app/src/main/java/com/example/apptranninghahalolo/MainActivity.java
+++ app/src/main/java/com/example/apptranninghahalolo/MainActivity.java
@@ -1,12 +1,29 @@
 package com.example.apptranninghahalolo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.utils.Display;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Formatter;
+import java.util.Locale;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements IBaseGpsListener {
 
     private Context context;
 
@@ -17,12 +34,104 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View view=findViewById(R.id.MainActivity);
+
         context=getApplicationContext();
 
         Display.Toast(context,"start project ...");
 
         Display.Log("start project ...");
 
+        //Display.SnackBar(view,"show snackbar...", Color.GREEN,Color.RED,20);
+
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        this.updateSpeed(null);
+
+        CheckBox chkUseMetricUntis = (CheckBox) this.findViewById(R.id.chkMetricUnits);
+        chkUseMetricUntis.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                MainActivity.this.updateSpeed(null);
+            }
+        });
+
+
+
+    }
+
+
+    public void finish()
+    {
+        super.finish();
+        System.exit(0);
+    }
+
+    private void updateSpeed(CLocation location) {
+        // TODO Auto-generated method stub
+        float nCurrentSpeed = 0;
+
+        if(location != null)
+        {
+            location.setUseMetricunits(this.useMetricUnits());
+            nCurrentSpeed = location.getSpeed();
+        }
+
+        Formatter fmt = new Formatter(new StringBuilder());
+        fmt.format(Locale.US, "%5.1f", nCurrentSpeed);
+        String strCurrentSpeed = fmt.toString();
+        strCurrentSpeed = strCurrentSpeed.replace(' ', '0');
+
+        String strUnits = "miles/hour";
+        if(this.useMetricUnits())
+        {
+            strUnits = "meters/second";
+        }
+
+        TextView txtCurrentSpeed = (TextView) this.findViewById(R.id.txtCurrentSpeed);
+        txtCurrentSpeed.setText(strCurrentSpeed + " " + strUnits);
+    }
+
+    private boolean useMetricUnits() {
+        // TODO Auto-generated method stub
+        CheckBox chkUseMetricUnits = (CheckBox) this.findViewById(R.id.chkMetricUnits);
+        return chkUseMetricUnits.isChecked();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+        if(location != null)
+        {
+            CLocation myLocation = new CLocation(location, this.useMetricUnits());
+            this.updateSpeed(myLocation);
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        // TODO Auto-generated method stub
 
     }
 }
